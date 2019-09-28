0977f9000ddfa61fabc4f1ae1876ef33c86ca6c3
bfazeli <bfazeli1@gmail.com>
2019-09-28 12:54:32 -0700

    Created addiditional files for further code abstraction

---
+++ MovieLover/Services/MovieStore.swift
@@ -0,0 +1,9 @@
+//
+//  MovieStore.swift
+//  MovieLover
+//
+//  Created by Bijan_Fazeli on 9/28/19.
+//  Copyright © 2019 Fazeli, Bijan. All rights reserved.
+//
+
+import Foundation
---
+++ MovieLover/Services/MovieStoreAPIError.swift
@@ -0,0 +1,9 @@
+//
+//  MovieStoreAPIError.swift
+//  MovieLover
+//
+//  Created by Bijan_Fazeli on 9/27/19.
+//  Copyright © 2019 Fazeli, Bijan. All rights reserved.
+//
+
+import Foundation
